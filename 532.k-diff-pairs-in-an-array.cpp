0b98c30ef588e8beddeb4b68aba9b5aead35e824
toanpi <htanvutoan012@gmail.com>
2022-11-28 09:29:08 +0700

    Test

---
+++ 532.k-diff-pairs-in-an-array.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=532 lang=cpp
+ *
+ * [532] K-diff Pairs in an Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        
+    }
+};
+// @lc code=end
+
